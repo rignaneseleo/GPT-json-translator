e245840fdec310169d77053f030a1af96b27d23d
Leo <dev.rignaneseleo@gmail.com>
2023-05-07 11:47:43 -0600

    on an existing file, translate only the missing rows

--- json_translator.py
+++ json_translator.py
@@ -20,10 +20,11 @@ print(f"Reading input file from {input_path}")
 
 # Load JSON file with language translations as a single string
 with open(input_path, "r") as f:
-    json = f.read()
+    source_json = json.load(f)
+    
 
 # Define function to translate text for a given target language
-def translate(target_language):
+def translate(target_language,rows_to_translate):
     print(f"Translating to {target_language}...")
     # Call OpenAI API to translate text
     completion = openai.ChatCompletion.create(
@@ -35,36 +36,57 @@ def translate(target_language):
             },
             {
                 "role": "user",
-                "content": f"Translate the following JSON to {target_language}:\n\n{json}\n",
+                "content": f"Translate the following JSON to {target_language}:\n\n{rows_to_translate}\n",
             },
         ],
     )
 
     translated_json = completion["choices"][0]["message"]["content"]
+    #only double quotes are allowed in JSON, so replace single quotes with double quotes
+    translated_json = translated_json.replace("'", '"')
     print(f"Translation to {target_language} complete.")
-    return target_language, translated_json
+    #print(f"Translated JSON:\n{translated_json}\n")
+    #convert the string into a json object
+    return json.loads(translated_json)
 
 
 # Translate the JSON string into target languages
 with concurrent.futures.ThreadPoolExecutor() as executor:
     # Submit translation tasks to the executor
-    future_to_language = {
-        executor.submit(translate, target_language): target_language
-        for target_language in languages
-    }
+    future_to_language = {}
+    for target_language in languages:
+        filename = f"{target_language}.json"
+        output_path = os.path.join(os.path.dirname(input_path), filename)
+        if os.path.exists(output_path):
+            with open(output_path, "r") as f:
+                existing_json = json.load(f)
+            existing_keys = "\n".join(existing_json.keys())
+            missing_keys = set(source_json.keys()) - set(existing_json.keys())
+            print(f"Found {len(missing_keys)} missing keys for {target_language}")
+            if missing_keys:
+                filtered_json = {key: value for key, value in source_json.items() if key in missing_keys}
+                #print(f"Filtered JSON:\n{filtered_json}\n")
+                future_to_language[executor.submit(translate, target_language, filtered_json)] = target_language, existing_json
+        else:
+            print(f"Output file not found for {target_language}. Generating a new one...")
+            future_to_language[executor.submit(translate, target_language, source_json)] = target_language, {}
+    
     # Process completed translation tasks and write output files
     for future in concurrent.futures.as_completed(future_to_language):
-        target_language = future_to_language[future]
+        target_language, existing_json = future_to_language[future]
         filename = f"{target_language}.json"
         try:
-            res = future.result()
+            translated_json = future.result()
+            if existing_json:
+                output_json = {**existing_json, **translated_json}
+            else:
+                output_json = json.loads(translated_json)
             output_path = os.path.join(os.path.dirname(input_path), filename)
             with open(output_path, "w") as f:
-                f.write(res[1])
+                json.dump(output_json, f, indent=2)
             print(f"Output file saved as {output_path}")
         except Exception as e:
-            print(
-                f"Error occurred while translating to {target_language}: {e}")
+            print(f"Error occurred while translating to {target_language}: {e}")
 
 print("Translation complete.")
 
